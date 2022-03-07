9a9fa83b98c1d3e2aff655bbc50244255025f644
Dylan Boyd <5061312+dylanjboyd@users.noreply.github.com>
2022-03-07 13:20:21 +1100

    Make home content dynamic and add life events

--- home/views.py
+++ home/views.py
@@ -5,4 +5,25 @@ from django.views.generic import TemplateView
 class HomeView(LoginRequiredMixin, TemplateView):
     """Shows the home page (for authenticated users only)"""
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["items"] = [
+            {
+                "title": "Medication",
+                "body": "Never miss a beat. Keep track of consumption and schedules for your meds.",
+                "path": "medication:medicine-index",
+            },
+            {
+                "title": "Body",
+                "body": "Measurements matter. Keep track of trends and make notes here.",
+                "path": "body:report-index",
+            },
+            {
+                "title": "Life Events",
+                "body": "Significant, small, or otherwise, it's probably worth remembering. Note it down here.",
+                "path": "life_events:event-index",
+            },
+        ]
+        return context
+
     template_name = "home/index.html"
