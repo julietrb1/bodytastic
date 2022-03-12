90702e0a2c5ba74abb62e75b63d3823ba4702d7f
Dylan Boyd <5061312+dylanjboyd@users.noreply.github.com>
2022-03-12 21:41:45 +1100

    Show Report created message

--- body/tests/test_report_create_view.py
+++ body/tests/test_report_create_view.py
@@ -1,11 +1,19 @@
 from django.urls import reverse
 from body.tests.login_test_case import LoginTestCase
-from body.urls.medication import MEDICINE_CREATE_ROUTE
+from body.urls.mybody import REPORT_CREATE_ROUTE
 from freezegun import freeze_time
+from django.utils.timezone import datetime, make_aware
 
 
-@freeze_time("2022-03-01")
+@freeze_time(make_aware(datetime(2022, 3, 1)))
 class MedicineCreateViewTests(LoginTestCase):
     def test_loads_form(self):
-        response = self.client.get(reverse(MEDICINE_CREATE_ROUTE))
+        response = self.client.get(reverse(REPORT_CREATE_ROUTE))
         self.assertContains(response, "Add")
+
+    def test_medicine_creation_shows_message(self):
+        response = self.client.post(
+            reverse(REPORT_CREATE_ROUTE), {"when": "March 2, 2022"}, follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assert_message(response, "Report Created")
